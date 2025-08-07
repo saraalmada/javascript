1957d7a22f1d3a57527b80ae6292cdc2ca75f1b4
saraalmada <saraalmadaj5@gmail.com>
2025-08-07 18:16:33 -0300

    finalizei atividade

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+// let c = 1
+// while (c < 6) {
+//     console.log(`Pizza ${c}`)
+//     c++
+// }
+
+// let c = 1
+// do {
+//     console.log(`Passo ${c}`)
+//     c++
+// } while (c < 6)
+
+console.log('Vai começar...')
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
+console.log('FIM!')
